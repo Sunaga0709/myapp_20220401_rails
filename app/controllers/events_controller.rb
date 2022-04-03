05d8db93442707c18a7e1a63d2f757411c5524f1
Sunaga0709 <ts19980709@gmail.com>
2022-04-03 14:50:26 +0900

    events_controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,44 @@
+class EventsController < ApplicationController
+  before_action :find_event, only: [:show, :update, :destroy]
+
+  def index
+    render json: Event.all
+  end
+
+  def show
+    render json: @event
+  end
+
+  def create
+    event = Event.new(event_params)
+    
+    if event.save
+      render json: event 
+    else
+      render json: event.errors, status: 422
+    end
+  end
+
+  def update
+    if @event.update(event_params)
+      render json: @event
+    else
+      render json: @event.errors, status: 422
+    end
+  end
+
+  def destroy
+    @event.destroy!
+    render json @event 
+  end
+
+  private 
+
+  def event_params
+    params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color)
+  end
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
+end
