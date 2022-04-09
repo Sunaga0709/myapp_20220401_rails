154254723f464989b037aa0746f212b5a95b1b66
Sunaga0709 <ts19980709@gmail.com>
2022-04-10 01:41:41 +0900

    rm devise

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,15 @@
 class EventsController < ApplicationController
   before_action :find_event, only: [:show, :update, :destroy]
 
-  def index # カレンダーに表示するイベントを返す
+  def index  # カレンダーに表示するイベントを返す
     render json: Event.all
   end
 
-  def show # 特定のイベントを返す
+  def show  # 特定のイベントを返す
     render json: @event
   end
 
-  def create # イベント作成
+  def create  # イベント作成
     event = Event.new(event_params)
     
     if event.save
@@ -19,7 +19,7 @@ class EventsController < ApplicationController
     end
   end
 
-  def update # 既存イベントを更新
+  def update  # 既存イベントを更新
     if @event.update(event_params)
       render json: @event
     else
@@ -27,18 +27,18 @@ class EventsController < ApplicationController
     end
   end
 
-  def destroy # イベント削除
+  def destroy  # イベント削除
     @event.destroy!
     render json: @event
   end
 
   private 
 
-  def event_params # 受け取る情報を制限
+  def event_params  # 受け取る情報を制限
     params.require(:event).permit(:id, :name, :start, :end, :memo, :color)
   end
 
-  def find_event # 特定のイベントを取得
+  def find_event  # 特定のイベントを取得
     @event = Event.find(params[:id])
   end
 end
