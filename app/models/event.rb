f4f6f6d839b0d227187980a2d9714b68b45dd5c0
Sunaga0709 <ts19980709@gmail.com>
2022-04-10 01:28:03 +0900

    add comment

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 100 }
-  validates :start, presence: true
-  validates :end, presence: true
+  validates :name, presence: true, length: { maximum: 100 } # 空不可、文字数制限
+  validates :start, presence: true # 空不可
+  validates :end, presence: true # 空不可
 end
