06e35ad43154d804241ebf0837cc525f3aea55c3
Sunaga0709 <ts19980709@gmail.com>
2022-04-13 09:55:15 +0900

    create registrations_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_03_050348) do
+ActiveRecord::Schema.define(version: 2022_04_13_003420) do
 
   create_table "events", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", limit: 100, null: false
@@ -20,6 +20,19 @@ ActiveRecord::Schema.define(version: 2022_04_03_050348) do
     t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "email"
+    t.text "tokens"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
